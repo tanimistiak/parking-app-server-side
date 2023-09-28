03b3599dedd16a0c876245b30bce0d15e868449e
tanimpersonal <tashintanim2019@gmail.com>
2023-09-28 21:48:13 +0600

    netlify-origin

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const cookieParser = require("cookie-parser");
 const baseRouter = require("./routes/api/v1/base.route");
 const app = express();
 const PORT = 8080;
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://rad-moonbeam-e18db0.netlify.app/",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 dbConnect();
 
 app.use("/api/v1/user", userRouter);
-app.use('/',baseRouter)
+app.use("/", baseRouter);
 app.get("/", (req, res) => {
   res.send("hello");
 });
